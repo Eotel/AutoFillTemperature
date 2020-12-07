2cbd3c5da95c96836c8c0400d85e98359cb2a34c
eotel <a20621@g.ichinoseki.ac.jp>
2020-12-07 15:20:21 +0900

    - 登校前や帰宅後のみの入力を実装 - 特定の日にちの体温の入力を実装

--- Readme.md
+++ Readme.md
@@ -21,23 +21,33 @@
 4. 設定ファイルを編集
     - settings.jsonを編集します
     
-    ```json
+    ```yaml
     {
         "user": "Microsoft アカウントのユーザー名（学籍番号）",
         "domain": "Microsoft アカウントのドメイン（@マークより後ろの部分）",
         "password": "Microsoft アカウントのパスワード",
         "url": "Formsのurlを入力します",
-        "morning_avg": 36.0,     <- 朝の平均体温
-        "evening_avg": 36.5,     <- 夜の平均体温
-        "deviation": 0.2,        <- 標準偏差
-        "temp_min": 35.6,        <- 最低体温
-        "temp_max": 37.3         <- 最高体温（これらのコメント文は削除してください）
+        "morning_avg": 36.0,     # <- 朝の平均体温
+        "evening_avg": 36.5,     # <- 夜の平均体温
+        "deviation": 0.2,        # <- 標準偏差
+        "temp_min": 35.6,        # <- 最低体温
+        "temp_max": 37.3         # <- 最高体温（これらのコメント文は削除してください）
     }
     ```
     
 5. 実行
     - `python3 main.py`
-    - ⚠️ 動作中にキーを入力すると適切に動作しない可能性があります
+    - ⚠️ 動作中にキーを入力すると適切に動作しない可能性があります 
+    - オプションの利用
+   
+   ```shell
+   usage: main.py [-h] [-d DATE] [-t TIME]
+
+   optional arguments:
+     -h, --help            show this help message and exit
+     -d DATE, --date DATE  Date to post, (e.g. "Today", "2020/01/12")
+     -t TIME, --time TIME  Time to post, (e.g. "am", "pm", and "both")
+   ```
 
 ## 環境
 
--- main.py
+++ main.py
@@ -4,9 +4,23 @@ from time import sleep
 
 import numpy as np
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+from argparse import ArgumentParser
 
 FILENAME = "settings.json"
+
+
+# オプションを受け取る
+def get_option():
+    parser = ArgumentParser()
+    parser.add_argument('-d', '--date', type=str,
+                        default='Today',
+                        help='Date to post, (e.g. "Today", "2020/01/12")')
+    parser.add_argument('-t', '--time', type=str,
+                        default='both',
+                        help='Time to post, (e.g. "am", "pm", and "both")')
+    return parser.parse_args()
 
 
 # 正規分布で体温を計算し閾値（最小・最大）外の場合は平均値に変更する
@@ -42,63 +56,79 @@ if __name__ == '__main__':
 
     t_mng = calc_temp(mng)
     t_evg = calc_temp(evg)
-
     temps = (t_mng, t_evg)
 
-    # 標準偏差の確認
-    # plt.figure()
-    # plt.hist(t_mng, bins=100, alpha=0.3, histtype='stepfilled', color='r')
-    # plt.hist(t_evg, bins=100, alpha=0.3, histtype='stepfilled', color='b')
-    # plt.show()
+    args = get_option()
+    time = args.time
+    date = args.date
+
+    isComplete = False  # 入力完了かどうか
+
+    browser = webdriver.Chrome()
+    wait = WebDriverWait(browser, 10)  # ToDo 要素が見つかるまで待機を実装する
+    browser.implicitly_wait(10)  # 暗黙的に10秒間まで待つ
 
     for i, t in enumerate(temps):
-        browser = webdriver.Chrome()
+        if isComplete is False:
+            # メールアドレスを入力
+            browser.get(url)
+            browser.find_element(By.ID, 'i0116').send_keys(email)
+            sleep(1)
+            browser.find_element(By.ID, 'idSIButton9').click()
 
-        # メールアドレスを入力
-        browser.get(url)
-        browser.find_element(By.ID, 'i0116').send_keys(email)
-        browser.find_element(By.ID, 'idSIButton9').click()
-        sleep(2)
+            # パスワードを入力
+            browser.find_element(By.ID, 'i0118').send_keys(pwd)
+            sleep(1)
+            browser.find_element(By.ID, 'idSIButton9').click()
+            sleep(1)
 
-        # パスワードを入力
-        browser.find_element(By.ID, 'i0118').send_keys(pwd)
-        browser.find_element(By.ID, 'idSIButton9').click()
-        sleep(2)
+            # サインイン状態を維持するか
+            browser.find_element(By.ID, 'idSIButton9').click()
 
-        # サインイン状態を維持するか
-        browser.find_element(By.ID, 'idSIButton9').click()
-        sleep(2)
+            # 本日の日付を入力
+            browser.find_element(By.XPATH, "//input[@class='office-form-question-textbox form-control "
+                                           "office-form-theme-focus-border border-no-radius "
+                                           "datepicker']").click()
+            if date == "Today":
+                browser.find_element(By.CLASS_NAME, "picker__button--today").click()
+            else:
+                browser.find_element(By.XPATH,
+                                     "//input[@class='office-form-question-textbox form-control "
+                                     "office-form-theme-focus-border border-no-radius datepicker']").send_keys(date)
+                browser.find_element(By.XPATH, "//button[@class='picker__button--close']").click()
 
-        # 本日の日付を入力
-        browser.find_element(By.XPATH, "//input[@class='office-form-question-textbox form-control "
-                                       "office-form-theme-focus-border border-no-radius "
-                                       "datepicker']").click()
-        browser.find_element(By.CLASS_NAME, "picker__button--today").click()
+            # 登校前か帰宅後か指定して次へ
+            if time == 'pm' or i == 1:
+                browser.find_element(By.XPATH, "//input[@value='帰宅（帰寮）後']").click()
+            elif time == 'am' or i == 0:
+                browser.find_element(By.XPATH, "//input[@value='登校前']").click()
 
-        # 登校前か帰宅後か指定して次へ
-        if i == 0:
-            browser.find_element(By.XPATH, "//input[@value='登校前']").click()
+            browser.find_element(By.XPATH, "//button[@title='次へ']").click()
+
+            # 体温を入力
+            if time == 'am':
+                t = temps[0]
+                isComplete = True
+            elif time == 'pm':
+                t = temps[1]
+                isComplete = True
+            browser.find_element(By.XPATH,
+                                 "//input[@class='office-form-question-textbox office-form-textfield-input form-control "
+                                 "office-form-theme-focus-border border-no-radius']").send_keys(str(t))
+
+            # 体調を入力
+            browser.find_element(By.XPATH, "//input[@value='良好']").click()
+
+            # 部活動を入力
+            if time == 'pm' or i == 1:
+                browser.find_element(By.CLASS_NAME, "select-placeholder").click()
+                browser.find_element(By.XPATH, "//*[text()=\"参加なし\"]").click()
+
+            # フォームを送信してタブを閉じる
+            browser.find_element(By.XPATH, "//button[@title='送信']").click()
+
         else:
-            browser.find_element(By.XPATH, "//input[@value='帰宅（帰寮）後']").click()
-        browser.find_element(By.XPATH, "//button[@title='次へ']").click()
-        sleep(2)
+            break
 
-        browser.find_element(By.XPATH,
-                             "//input[@class='office-form-question-textbox office-form-textfield-input form-control "
-                             "office-form-theme-focus-border border-no-radius']").send_keys(str(t))
-
-        # 体調を入力
-        browser.find_element(By.XPATH, "//input[@value='良好']").click()
-
-        # 部活動を入力
-        if i == 1:
-            browser.find_element(By.CLASS_NAME, "select-placeholder").click()
-            browser.find_element(By.XPATH, "//*[text()=\"参加なし\"]").click()
-
-        # フォームを送信してタブを閉じる
-        browser.find_element(By.XPATH, "//button[@title='送信']").click()
-        sleep(1)
-
-        if i == 1:
-            browser.close()
-            browser.quit()
+    browser.close()
+    browser.quit()
